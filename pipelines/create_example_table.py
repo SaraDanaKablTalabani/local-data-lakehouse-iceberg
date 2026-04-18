ef1e99a686e98e3397771443411f383a8f832187
Sara Dana Kabl Talabani <saradana.talabani@unisalento.it>
2026-04-18 12:51:20 +0200

    Initial commit

--- pipelines/create_example_table.py
+++ pipelines/create_example_table.py
@@ -1,32 +1,35 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, LongType, TimestampType
-from datetime import datetime
 
 
 def create_table(spark: SparkSession) -> None:
-    schema = StructType(
-        [
-            StructField("id", LongType(), False),
-            StructField("name", StringType(), True),
-            StructField("age", IntegerType(), True),
-            StructField("created_datetime", TimestampType(), True),
-        ]
+
+
+    df = (
+        spark.read
+        .option("header", True)
+        .option("inferSchema", True)
+        .csv("/opt/spark/apps/assets/cars.csv")
+        .limit(5)
     )
 
-    data = [
-        (1, "Alice", 30, datetime.now()),
-        (2, "Bob", 25, datetime.now()),
-        (3, "Charlie", 35, datetime.now()),
-    ]
 
-    (
-        spark
-        .createDataFrame(data, schema)
-        .write.format("iceberg")
-        .mode("overwrite")
-        .saveAsTable("catalog_iceberg.schema_iceberg.table_iceberg")
-    )
+
+    #df = (
+     #   spark.read
+     #   .option("header", True)
+      #  .option("inferSchema", True)
+      #  .csv("/opt/spark/apps/assets/cars.csv")
+   # )
+
+    df.write \
+        .format("iceberg") \
+        .mode("overwrite") \
+        .saveAsTable("catalog_iceberg.schema_iceberg.cars")
+
 
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("SparkSessionExample").getOrCreate()
+    spark = SparkSession.builder \
+        .appName("CarsPipeline") \
+        .getOrCreate()
+
     create_table(spark)
